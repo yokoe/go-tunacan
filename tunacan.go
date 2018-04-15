13f8d7e16e3f91c7338478038cf8dd4667505d34
Sota Yokoe <sota@kreuz45.com>
2018-04-15 22:51:15 +0900

    feat: Concat images

--- tunacan.go
+++ tunacan.go
@@ -4,10 +4,12 @@ import (
 	"flag"
 	"fmt"
 	"image"
+	"image/draw"
+	"image/jpeg"
 	"os"
 )
 
-import _ "image/jpeg"
+import _ "image/png"
 
 // https://stackoverflow.com/questions/28322997/how-to-get-a-list-of-values-into-a-flag-in-golang?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 type arrayFlags []string
@@ -29,15 +31,15 @@ func main() {
 	flag.StringVar(&outputFilename, "o", "", "Output filepath.")
 	flag.Parse()
 
-	for i := range sourceFilenames {
-		fmt.Println("Source: ", sourceFilenames[i])
-	}
-	fmt.Println("Output: ", outputFilename)
-
-	concat(sourceFilenames)
+	concat(sourceFilenames, outputFilename)
 }
 
-func concat(sourceFilenames []string) {
+func concat(sourceFilenames []string, outputFilename string) {
+	canvasWidth := 0
+	canvasHeight := 0
+
+	images := []image.Image{}
+
 	for i := range sourceFilenames {
 		filename := sourceFilenames[i]
 		fmt.Println(filename)
@@ -50,6 +52,39 @@ func concat(sourceFilenames []string) {
 			panic(err)
 		}
 
+		images = append(images, srcImg)
+
+		canvasWidth += srcImg.Bounds().Size().X
+		canvasHeight = Max(canvasHeight, srcImg.Bounds().Size().Y)
+
 		fmt.Println(srcImg.Bounds().Size())
 	}
+
+	fmt.Println("Canvas size: ", canvasWidth, canvasHeight)
+
+	outputImage := image.NewRGBA(image.Rect(0, 0, canvasWidth, canvasHeight))
+
+	x := 0
+	for i := range images {
+		srcImage := images[i]
+
+		srcRect := image.Rect(x, 0, srcImage.Bounds().Size().X+x, srcImage.Bounds().Size().Y)
+		draw.Draw(outputImage, srcRect, srcImage, image.Pt(0, 0), draw.Src)
+
+		x += srcImage.Bounds().Size().X
+	}
+
+	file, _ := os.Create(outputFilename)
+	defer file.Close()
+
+	if err := jpeg.Encode(file, outputImage, &jpeg.Options{100}); err != nil {
+		panic(err)
+	}
+}
+
+func Max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
